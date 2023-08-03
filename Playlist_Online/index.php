b0a6a9b88efdc22f2c6a4120cdf8036c210c4a36
Igor_Mattos_Motta <68616440+IgorMattosMotta@users.noreply.github.com>
2023-08-03 04:33:26 -0300

    Update index.php

--- Playlist_Online/index.php
+++ Playlist_Online/index.php
@@ -21,7 +21,6 @@
 		<div id='resto'>
 			<h2 class='h2_resto'>Para que serve esse site?</h2>
 			<p class='p_resto'>A criação desse site tem como intuito de expor músicas e gêneros musicais com suas devidas informações!
-			   Além de receber a pontução máxima, é nois Dedé =)
 			</p>
 			<img id='img_dancarinos' src="css/imagens/dancarinos.jpg" alt="Dançarinos atuando">
 			<h2 class=h2_resto>Como utilizar o site?</h2>
